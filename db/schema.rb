438ebfc94d99ca2e22cd3e0e0c9bb91a13585186
Amelia Risner <amelia.risner0@gmail.com>
2023-06-09 23:39:12 -0700

    database sucessfully converted to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_03_07_221959) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.float "want_hot"
