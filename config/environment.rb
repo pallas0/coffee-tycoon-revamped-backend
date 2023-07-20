34c496d7db63b38dc2771559c878efed7342333e
Amelia Risner <amelia.risner0@gmail.com>
2023-07-19 17:39:33 -0700

    altered env file

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,5 @@
+require 'faker'
+I18n.reload!
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "development"
@@ -8,6 +10,3 @@ Bundler.require(:default, ENV['RACK_ENV'])
 
 # Require in all files in 'app' directory
 require_all 'app'
-
-#added for Heroku connect
-#set :database_file, “./database.yml”
